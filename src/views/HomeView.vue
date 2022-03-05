c462e51ef82abc69a136898b916c46fae8b261bb
maeve-du <98177760+maeve-du@users.noreply.github.com>
2022-03-05 19:25:49 +0800

    Initial commit
    Cleaned vue cli init components, layout
    Changed ESlint default rules,  to prevent unnecessary warning
    Finished HomeHeader component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <HomeHeader />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HomeHeader from "@/components/HomeHeader.vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { HomeHeader },
+};
 </script>
